91e54dae63634cf3bb340839bad728ba00dad0df
Zui Shen <shenzui_da@hotmail.com>
2020-09-16 23:36:06 +0800

    Create PageCrawler.java

---
+++ PageCrawler.java
@@ -0,0 +1,13 @@
+package com.zboot.cleancode.chapter03;
+
+public class PageCrawler {
+
+    public WikiPagePath getFullpath(WikiPage suiteSetup) {
+        return null;
+    }
+
+    public WikiPagePath getFullPath(WikiPage setup) {
+        return null;
+    }
+
+}
