b7845e84519d2a49feabeca1824eb99afc1f5578
Zui Shen <shenzui_da@hotmail.com>
2020-09-16 23:42:16 +0800

    Create SuiteResponder.java

---
+++ SuiteResponder.java
@@ -0,0 +1,8 @@
+package com.zboot.cleancode.chapter03;
+
+public class SuiteResponder {
+
+    public static final String SUITE_SETUP_NAME = null;
+    public static final String SUITE_TEARDOWN_NAME = null;
+
+}
