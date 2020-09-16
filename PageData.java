299b5ee8f2d8ed2e31b183b398f7608974a15075
Zui Shen <shenzui_da@hotmail.com>
2020-09-16 23:41:32 +0800

    Create PageData.java

---
+++ PageData.java
@@ -0,0 +1,25 @@
+package com.zboot.cleancode.chapter03;
+
+public class PageData {
+
+    public WikiPage getwikiPage() {
+        return null;
+    }
+
+    public boolean hasAttribute(String string) {
+        return false;
+    }
+
+    public Object getContent() {
+        return null;
+    }
+
+    public void setContent(String string) {
+
+    }
+
+    public String getHtml() {
+        return null;
+    }
+
+}
