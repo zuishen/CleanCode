bebdfb70de6f5ffa55a79842ad8653b6cfbf47ba
Zui Shen <shenzui_da@hotmail.com>
2020-09-16 23:40:43 +0800

    Create PageCrawlerImpl.java

---
+++ PageCrawlerImpl.java
@@ -0,0 +1,9 @@
+package com.zboot.cleancode.chapter03;
+
+public class PageCrawlerImpl {
+
+    public static WikiPage getInheritedPage(String suiteSetupName, WikiPage wikiPage) {
+        return null;
+    }
+
+}
